fde1b04f1c0ac74e1d5b7af79d103dce2dc553fa
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-12-10 14:13:39 +0100

    Step 2 (cont'd): add more sites to connect to concurrently.

--- web-crawler/non-blocking-sockets-selectors.py
+++ web-crawler/non-blocking-sockets-selectors.py
@@ -2,21 +2,46 @@
 import socket
 from contextlib import suppress
 from selectors import DefaultSelector, EVENT_WRITE
+from functools import wraps
+
+
+def connected_to_xkcd_eventhandler():
+    print('connected to xkcd.com')
+
+def connected_to_oeis_eventhandler():
+    print('connected to oeis.org')
+
+def connected_to_google_eventhandler():
+    print('connected to google.com')
+    
+sites = {   ('xkcd.com', 80): connected_to_xkcd_eventhandler,
+            ('oeis.org', 80): connected_to_oeis_eventhandler, 
+            ('google.com', 80): connected_to_google_eventhandler, }
+
+def prepare_sockets(sites, selector):
+
+    sockets = {} 
+    for site, callback in sites.items():
+
+        sock = socket.socket()
+        sock.setblocking(False)
+
+        @wraps(callback)
+        def unregister_and_call(sock=sock, callback=callback):
+            selector.unregister(sock.fileno())
+            callback()
+
+        selector.register(sock.fileno(), EVENT_WRITE, unregister_and_call)
+
+        sockets[site] = sock
+
+    return sockets
 
 selector = DefaultSelector()
 
-sock = socket.socket()
-sock.setblocking(False)
-
-def connected_eventhandler():
-    selector.unregister(sock.fileno())
-    print('connected')
-
-# we want to know when the socket is "writable"
-selector.register(sock.fileno(), EVENT_WRITE, connected_eventhandler) 
-
-with suppress(BlockingIOError):
-    sock.connect(('xkcd.com', 80))
+for site, sock in prepare_sockets(sites, selector).items():
+    with suppress(BlockingIOError):
+        sock.connect(site)
 
 def loop():
     while True:
@@ -32,3 +57,7 @@ def loop():
 
 with suppress(KeyboardInterrupt):
     loop()
+
+# We have achieved "concurrency" here, but not what is traditionally called "parallelism". 
+# That is, we built a tiny system that does overlapping I/O. It is capable of beginning 
+# new operations while others are in flight.
