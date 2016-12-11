660a5394b9a2012e20f19454ee7ea37d55629db4
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-12-11 17:12:36 +0100

    Extract `unregister` decorator.

--- web-crawler/non-blocking-sockets-selectors.py
+++ web-crawler/non-blocking-sockets-selectors.py
@@ -5,51 +5,38 @@ from selectors import DefaultSelector, EVENT_WRITE
 from functools import wraps
 
 
-def connected_to_xkcd_eventhandler():
-    print('connected to xkcd.com')
+def unregister(selector, key=lambda sock: sock.fileno()):
 
-def connected_to_oeis_eventhandler():
-    print('connected to oeis.org')
+    def U(decorated):
 
-def connected_to_google_eventhandler():
-    print('connected to google.com')
-    
-sites = {   ('xkcd.com', 80): connected_to_xkcd_eventhandler,
-            ('oeis.org', 80): connected_to_oeis_eventhandler, 
-            ('google.com', 80): connected_to_google_eventhandler, }
+        def make_callback(sock):
+
+            @wraps(decorated)
+            def callback(*args, **kwds):
+                selector.unregister(key(sock))
+                return decorated(*args, **kwds)
+
+            return callback
+
+        return make_callback
+
+    return U
 
 def prepare_sockets(sites, selector):
 
-    def unregister_and_call(sock, callback):
-
-        @wraps(callback)
-        def wrapper():
-            selector.unregister(sock.fileno())
-            callback()
-
-        return wrapper
-
     sockets = {} 
-    for site, callback in sites.items():
+    for site, handler in sites.items():
 
         sock = socket.socket()
         sock.setblocking(False)
         
-        selector.register(  sock.fileno(), 
-                            EVENT_WRITE, 
-                            unregister_and_call(sock, callback))
+        selector.register(sock.fileno(), EVENT_WRITE, handler(sock))
 
         sockets[site] = sock
 
     return sockets
 
-selector = DefaultSelector()
-
-for site, sock in prepare_sockets(sites, selector).items():
-    with suppress(BlockingIOError):
-        sock.connect(site)
-
-def loop():
+def loop(selector):
     while True:
         # Unlike in our fast-spinning loop above, the call to `select` 
         # here pauses, awaiting the next I/O events. 
@@ -61,8 +48,30 @@ def loop():
 # An async framework builds on the two features we have shown:
 # non-blocking sockets and the event loop, to run concurrent operations on a single thread.
 
+selector = DefaultSelector()
+
+@unregister(selector)
+def connected_to_xkcd_eventhandler():
+    print('connected to xkcd.com')
+
+@unregister(selector)
+def connected_to_oeis_eventhandler():
+    print('connected to oeis.org')
+
+@unregister(selector)
+def connected_to_google_eventhandler():
+    print('connected to google.com')
+
+sites = {   ('xkcd.com', 80): connected_to_xkcd_eventhandler,
+            ('oeis.org', 80): connected_to_oeis_eventhandler, 
+            ('google.com', 80): connected_to_google_eventhandler, }
+
+for site, sock in prepare_sockets(sites, selector).items():
+    with suppress(BlockingIOError):
+        sock.connect(site)
+
 with suppress(KeyboardInterrupt):
-    loop()
+    loop(selector)
 
 # We have achieved "concurrency" here, but not what is traditionally called "parallelism". 
 # That is, we built a tiny system that does overlapping I/O. It is capable of beginning 
