8860626034017322a0ab2722a1786e0f7ece6db0
Massimo Nocentini <massimo.nocentini@gmail.com>
2017-01-02 09:59:58 +0100

    Finish to work on `typely` code.

--- beazley-metaprogramming/typely/typely.py
+++ beazley-metaprogramming/typely/typely.py
@@ -6,6 +6,8 @@ from inspect import Parameter, Signature
 import re
 from collections import OrderedDict
 
+# It is possible to compose the following descriptors as *mixins*; the correct
+# behavior is ensure by `super()` invocation and mandatory keyword arguments.
 class Descriptor:
     def __init__(self, name=None):
         self.name = name
@@ -64,6 +66,13 @@ class SizedString(String, Sized):
 
 # Pattern matching
 class Regex(Descriptor):
+    '''
+    Doctest
+
+    >>> Regex.__mro__
+    (<class 'typely.Regex'>, <class 'typely.Descriptor'>, <class 'object'>)
+    '''
+
     def __init__(self, *args, pat, **kwargs):
         self.pat = re.compile(pat)
         super().__init__(*args, **kwargs)
@@ -74,6 +83,12 @@ class Regex(Descriptor):
         super().__set__(instance, value)
 
 class SizedRegexString(SizedString, Regex):
+    '''
+    Doctest
+
+    >>> SizedRegexString.__mro__
+    (<class 'typely.SizedRegexString'>, <class 'typely.SizedString'>, <class 'typely.String'>, <class 'typely.Typed'>, <class 'typely.Sized'>, <class 'typely.Regex'>, <class 'typely.Descriptor'>, <class 'object'>)
+    '''
     pass
 
 # Structure definition code
@@ -84,19 +99,21 @@ def make_signature(names):
         for name in names)
 
 class StructMeta(type):
+
     @classmethod
     def __prepare__(cls, name, bases):
-        return OrderedDict()
+        '''Creates and returns a dictionary to use for execution of the class body'''
+        return OrderedDict() # will preserve attrs order, as objs appears in body
 
     def __new__(cls, clsname, bases, clsdict):
-        fields = [key for key, val in clsdict.items()
-                  if isinstance(val, Descriptor) ]
+
+        # collect descriptors and set their names
+        fields = [key for key, val in clsdict.items() if isinstance(val, Descriptor)]
         for name in fields:
             clsdict[name].name = name
 
-        clsobj = super().__new__(cls, clsname, bases, dict(clsdict))
-        sig = make_signature(fields)
-        setattr(clsobj, '__signature__', sig)
+        clsobj = super().__new__(cls, clsname, bases, dict(clsdict)) # it seems to work even without `dict` 
+        setattr(clsobj, '__signature__', make_signature(fields))
         return clsobj
 
 class Structure(metaclass=StructMeta):
@@ -105,11 +122,55 @@ class Structure(metaclass=StructMeta):
         for name, val in bound.arguments.items():
             setattr(self, name, val)
 
-if __name__ == '__main__':
-    class Stock(Structure):
-        name = SizedRegexString(maxlen=8, pat='[A-Z]+$')
-        shares = PosInteger()
-        price = PosFloat()
+class Stock(Structure):
+    '''
+    Doctests
+
+    >>> s = Stock('GOOG', 100, 490.1) # everything ok
+    >>> s.name = 123 # expect type error
+    Traceback (most recent call last):
+      File "/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/doctest.py", line 1330, in __run
+        compileflags, 1), test.globs)
+      File "<doctest typely.Stock[1]>", line 1, in <module>
+        s.name = 123
+      File "/Users/mn/Developer/working-copies/pythons/on-python/beazley-metaprogramming/typely/typely.py", line 23, in __set__
+        raise TypeError('Expected %s' % self.ty)
+    TypeError: Expected <class 'str'>
+    >>> s = Stock('G_OG', 100, 490.1) # expect string error
+    Traceback (most recent call last):
+      File "/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/doctest.py", line 1330, in __run
+        compileflags, 1), test.globs)
+      File "<doctest typely.Stock[1]>", line 1, in <module>
+        s = Stock('G_OG', 100, 490.1) # everything ok
+      File "/Users/mn/Developer/working-copies/pythons/on-python/beazley-metaprogramming/typely/typely.py", line 109, in __init__
+        setattr(self, name, val)
+      File "/Users/mn/Developer/working-copies/pythons/on-python/beazley-metaprogramming/typely/typely.py", line 24, in __set__
+        super().__set__(instance, value)
+      File "/Users/mn/Developer/working-copies/pythons/on-python/beazley-metaprogramming/typely/typely.py", line 60, in __set__
+        super().__set__(instance, value)
+      File "/Users/mn/Developer/working-copies/pythons/on-python/beazley-metaprogramming/typely/typely.py", line 73, in __set__
+        raise ValueError('Invalid string')
+    ValueError: Invalid string
+    >>> s = Stock('GOOG', -2, 490.1) # expect int error
+    Traceback (most recent call last):
+      File "/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/doctest.py", line 1330, in __run
+        compileflags, 1), test.globs)
+      File "<doctest typely.Stock[2]>", line 1, in <module>
+        s = Stock('GOOG', -2, 490.1) # expect string error
+      File "/Users/mn/Developer/working-copies/pythons/on-python/beazley-metaprogramming/typely/typely.py", line 109, in __init__
+        setattr(self, name, val)
+      File "/Users/mn/Developer/working-copies/pythons/on-python/beazley-metaprogramming/typely/typely.py", line 24, in __set__
+        super().__set__(instance, value)
+      File "/Users/mn/Developer/working-copies/pythons/on-python/beazley-metaprogramming/typely/typely.py", line 40, in __set__
+        raise ValueError('Expected >= 0')
+    ValueError: Expected >= 0
+    '''
+
+    # the following ordering of attributes is preserved by `OrderedDict` in
+    # `__prepare__` method in class `StructMeta`.
+    name = SizedRegexString(maxlen=8, pat='[A-Z]+$')
+    shares = PosInteger()
+    price = PosFloat()
 
 
 
