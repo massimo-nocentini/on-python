12d379d143f84a2d0be347d666a0a27ea8f38940
Massimo Nocentini <massimo.nocentini@gmail.com>
2019-05-01 11:47:56 +0200

    Implement topological sort.

--- greedys/interval-partitioning.py
+++ greedys/interval-partitioning.py
@@ -1,35 +1,73 @@
 
 from collections import namedtuple, defaultdict
+import heapq
 
-job = namedtuple('job', ['start_time', 'duration', 'deadline', 'name']) # `name` in last position to use `<` on tuples for ordering.
+def dag(jobs):
+
+    D = defaultdict(set)
+
+    for j in jobs:
+        for p in j.deps:
+            D[p].add(j.name) # revert the connections
+
+    return D
+
+def topological_sort(jobs):
+
+    forward_deps = dag(jobs)
+
+    q = [] # the priority queue
+
+    for job in jobs:
+        heapq.heappush(q, (len(job.deps), job))
+
+    while q:
+
+        _, job = heapq.heappop(q)
+
+        yield job
+
+        dependents = forward_deps[job.name]
+
+        if not dependents: continue
+
+        for i in range(len(q)):
+            priority, J = q[i]
+            if J.name in dependents:
+                q[i] = (priority - 1, J) 
+        heapq.heapify(q) 
+
+job = namedtuple('job', ['start_time', 'duration', 'deadline', 'name', 'deps']) # `name` in last position to use `<` on tuples for ordering.
 
 def finish_time(jb):
     return jb.start_time + jb.duration
 
 jobs = [ # some overlapping jobs...
-    job(0, 3, 3, 'A'),
-    job(0, 7, 7, 'B'),
-    job(0, 3, 3, 'C'),
-    job(4, 3, 10, 'D'),
-    job(4, 6.5, 13.5, 'E'),
-    job(9, 3, 12, 'F'),
-    job(9, 3, 12, 'G'),
-    job(11.5, 4.5, 16, 'H'),
-    job(13, 3, 16, 'I'),
-    job(13, 3, 16, 'J'),
+    job(0, 3, 3, 'A', []),
+    job(0, 7, 7, 'B', []),
+    job(0, 3, 3, 'C', ['A']),
+    job(4, 3, 10, 'D', ['B', 'C']),
+    job(4, 6.5, 13.5, 'E', ['A']),
+    job(9, 3, 12, 'F', ['C']),
+    job(9, 3, 12, 'G', ['D']),
+    job(11.5, 4.5, 16, 'H', ['C', 'G']),
+    job(13, 3, 16, 'I', ['F', 'G']),
+    job(13, 3, 16, 'J', ['H']),
 ] # every job ends at time 16.
+
+print(list(topological_sort(jobs)))
 
 label = {J.name: {'M₀', 'M₁', 'M₂', 'M₃', 'M₄'} for J in jobs} # each job can be assigned to any machine, initially.
 label['A'] = {'M₃'} # job 'E' can be performed on the first machine only.
 label['E'] = {'M₀'} # job 'E' can be performed on the first machine only.
 label['D'] = {'M₁', 'M₂'} # job 'E' can be performed on the first machine only.
 
-ordering = list(enumerate(sorted(jobs)))
+ordering = list(topological_sort(jobs))
 
 busy = defaultdict(list)
 busy.update({
-    'M₀': [job(2, 3, None, 'cleaning'), job(14, 1, None, 'sunday')],
-    'M₁': [job(5, 2, None, 'maintenance')],
+    'M₀': [job(2, 3, None, 'cleaning', []), job(14, 1, None, 'sunday', [])],
+    'M₁': [job(5, 2, None, 'maintenance', [])],
 })
 
 def overlaps(I, J):
@@ -42,10 +80,10 @@ def run(prefix, jobs, machine, label):
 
     if jobs: # still jobs to allocate.
 
-        (j, J), *Js = jobs # unpacking.
+        J, *Js = jobs # unpacking.
         L = label[J.name].copy()
 
-        for i, I in prefix: # O(n^2) complexity because of the last job; btw, preprocess of overlappings may help to check only those ones, getting a linear time.
+        for I in prefix: # O(n^2) complexity because of the last job; btw, preprocess of overlappings may help to check only those ones, getting a linear time.
             if overlaps(I, J):
                 label[J.name] -= {machine[I.name]} # remove the machine on which job `I` is allocated for possibilities about job `J`.
 
@@ -60,7 +98,7 @@ def run(prefix, jobs, machine, label):
 
             if ontime(J_delayed):
                 machine[J.name] = l # an attempt to allocate job `J` on machine `l`.
-                yield from run([(j, J_delayed)] + prefix, Js, machine.copy(), label)
+                yield from run([J_delayed] + prefix, Js, machine.copy(), label)
 
         label[J.name] = L
     else:
@@ -71,7 +109,7 @@ def sol_handler(sol):
     
     machine, prefix = sol
     M = {m: [] for m in set(machine.values())}
-    for j, J in prefix:
+    for J in prefix:
         M[machine[J.name]].append(J)
     for k, v in M.items():
         v.sort()
